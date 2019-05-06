ce2c756dfe58e1c42bce63ed3bdb25e97ac0ecac
Nevo0 <Piecyk.rafal@gmail.com>
2019-05-06 08:55:40 +0200

    Leyout

---
+++ src/pages/Rooms.js
@@ -0,0 +1,6 @@
+import React from "react";
+// import Hero from "../Components/Hero";
+import Hero from "../Components/Hero";
+export default function Rooms() {
+  return <Hero hero="roomsHero" />;
+}
