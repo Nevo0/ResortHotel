eeda777ea68241b1e71d28f9d6d4a6373936f5b6
Nevo0 <Piecyk.rafal@gmail.com>
2019-05-06 16:03:05 +0200

    Featured Rooms

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export default function Room({ room }) {
@@ -7,7 +8,26 @@ export default function Room({ room }) {
 
   return (
     <article className="room">
-      <h1>a</h1>
+      <div className="img-container">
+        <img src={images[0]} alt={name} />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
     </article>
   );
 }
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string),
+    price: PropTypes.number.isRequired
+  })
+};
